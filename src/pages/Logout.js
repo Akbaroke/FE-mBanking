1537625c18d8d6a1a88793c08d1c84bfcefd9796
akbaroke <akbarmuhammad833@gmail.com>
2023-01-11 23:25:28 +0700

    update

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 async function Logout() {
   try {
-    await axios.delete('http://localhost:5000/logout')
+    await axios.delete(process.env.API+'logout')
   } catch (error) {
     console.log(error);
   }
